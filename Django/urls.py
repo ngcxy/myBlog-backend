fb8f39b1f7431637a975e5027e8d0809a718abb1
ngcxy <caixinyi2013@hotmail.com>
2023-07-18 21:20:16 -0700

    image on s3

--- Django/urls.py
+++ Django/urls.py
@@ -29,6 +29,4 @@ urlpatterns = [
     ), name='openapi-schema'),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
