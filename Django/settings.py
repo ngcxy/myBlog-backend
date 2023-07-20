c4dd4d6184fefaddfb3ad091b51163f23bdd037c
ngcxy <caixinyi2013@hotmail.com>
2023-07-19 21:25:06 -0700

    static file

--- Django/settings.py
+++ Django/settings.py
@@ -177,7 +177,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-MEDIA_ROOT = BASE_DIR / 'photo'
+MEDIA_ROOT = BASE_DIR / 'static/photo'
 MEDIA_URL = 'https://blog-image-ngcxy.s3.amazonaws.com/'
 
 STORAGES = {
--- Django/urls.py
+++ Django/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     ), name='openapi-schema'),
 ]
 
-# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
