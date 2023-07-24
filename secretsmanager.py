a15b2c8465b19f11f1e974fdf9043a4faca26eb9
ngcxy <caixinyi2013@hotmail.com>
2023-07-23 18:27:03 -0700

    access key

--- secretsmanager.py
+++ secretsmanager.py
@@ -1,16 +1,11 @@
 from cryptography.fernet import Fernet
 
-encryption_key = Fernet.generate_key()
+key = Fernet.generate_key()
+fernet = Fernet(key)
 
 access_key = "b'gAAAAABkvdCGkNb0VEtUXvixXkWzr279fTBzRL1qbIwRc0vEOJmiWAB0QlvMjrdUnwUnem4XPqZzCJM_wUMXARDDn4f_CquHYN2g5AcTNUDixiMNy27usbY='"
 secret_key = "b'gAAAAABkvdEtWyqYl-7Erst4FhzPYuWjTjW9HyoQOS6Y980wYldz0jyd1TIWzabMK5wkN0XbDGY8HA4XfxglwGiSaGK4hpRxG9Q8D_cM-4p4nZZDZFrIS11aztFWiTF3BGd2cbMlzItg'"
 
 
-def decrypt_secret(encrypted_secret):
-    f = Fernet(encryption_key)
-    decrypted_secret = f.decrypt(encrypted_secret).decode()
-    return decrypted_secret
-
-
-decrypted_access_key = decrypt_secret(access_key)
-decrypted_secret_key = decrypt_secret(secret_key)
+decrypted_access_key = fernet.decrypt(access_key).decode()
+decrypted_secret_key = fernet.decrypt(secret_key).decode()
