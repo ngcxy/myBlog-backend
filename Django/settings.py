5db433e9ee23dd70f200fec37df8d948879d6ea9
ngcxy <caixinyi2013@hotmail.com>
2023-07-23 23:02:21 -0700

    solve not found

--- Django/settings.py
+++ Django/settings.py
@@ -178,15 +178,15 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_ROOT = BASE_DIR / 'static/media'
 MEDIA_URL = 'https://blog-image-ngcxy.s3.amazonaws.com/'
-
-STORAGES = {
-    "default": {"BACKEND": "storages.backends.s3boto3.S3Boto3Storage"},
-    "staticfiles": {"BACKEND": "storages.backends.s3boto3.S3StaticStorage"}
-}
-
-
-AWS_QUERYSTRING_AUTH = False
-AWS_ACCESS_KEY_ID = smanager.access_key
-AWS_SECRET_ACCESS_KEY = smanager.secret_key
-AWS_STORAGE_BUCKET_NAME = 'blog-image-ngcxy'
+#
+# STORAGES = {
+#     "default": {"BACKEND": "storages.backends.s3boto3.S3Boto3Storage"},
+#     "staticfiles": {"BACKEND": "storages.backends.s3boto3.S3StaticStorage"}
+# }
+#
+#
+# AWS_QUERYSTRING_AUTH = False
+# AWS_ACCESS_KEY_ID = smanager.access_key
+# AWS_SECRET_ACCESS_KEY = smanager.secret_key
+# AWS_STORAGE_BUCKET_NAME = 'blog-image-ngcxy'
 
