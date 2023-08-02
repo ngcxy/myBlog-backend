9c5f7974cfdf9873938696596504147186ec0c4a
ngcxy <caixinyi2013@hotmail.com>
2023-08-01 22:49:22 -0700

    category key

--- blog/models.py
+++ blog/models.py
@@ -10,6 +10,7 @@ def upload_to(instance, filename):
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
+    id = models.BigAutoField(primary_key=True)
 
     def __str__(self):
         return self.name
